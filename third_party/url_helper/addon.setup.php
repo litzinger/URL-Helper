063bc0565742c14cf6115d8e1dac53926c7f78e8
Brian Litzinger <litzinger@gmail.com>
2017-04-24 12:01:40 -0500

    Create addon.setup.php

---
+++ third_party/url_helper/addon.setup.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'author'        => 'BoldMinded',
+    'author_url'    => 'https://boldminded.com',
+    'docs_url'      => 'https://github.com/litzinger/url-helper',
+    'name'          => 'Url Helper',
+    'description'   => '',
+    'version'       => '1.0.8',
+    'namespace'     => 'BoldMinded\UrlHelper',
+    'settings_exist' => false,
+];
